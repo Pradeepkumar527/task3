51b4292c50f17af46ec3d00deec34be58e58de57
Padeepkumar527 <Padeepkumargulipalli99@gmail.com>
2020-06-20 15:50:44 +0530

    added all files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import data from './data/data.json';
+import './css/bootstrap.min.css';
+import profilepic from './1077114.svg';
 
 function App() {
+  let profiles=data.profile;
+  console.log(profiles);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className="row justify-content-center">
+   
+    {profiles.map((value)=>( 
+       <div className="col-lg-4 col-md-6 col-sm-10 mt-5">
+     
+        <div className="card" style={{width:"25rem",fontStyle:"italic",boxShadow:"0px 8px 8px -8px #000"}}>
+            <img  src={profilepic}  />
+            <div className="card-body text-center">
+                 <h1>{value.basics.name}</h1>
+                 <div className="card-text">
+                         <h4> {value.basics.role}</h4>
+                 </div>
+                 <a href={"tel"+value.basics.phone}>{value.basics.phone}</a>
+                 <a href={"mailto"+value.basics.email}>{value.basics.email}</a>
+               
+                 <button className="btn btn-primary">View Profile</button>
+            </div>
+        </div>
+
+
+     </div>))
+   }
+   
+    
+   </div>
+    )}
+
+
+    
+    
+
+
 
 export default App;
+
---
+++ src/StatesConcept.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+
+class StatesConcept extends Component{
+	
+	render(){
+		return(
+				<div>
+					<h2>StatesConcept component</h2>
+				</div>
+			)
+	}
+}
+
+export default StatesConcept;
